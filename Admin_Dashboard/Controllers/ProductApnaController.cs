452696035a0484bb5dfe98f519692e91e6a2ec4c
Champika Tripathi <Champika Tripathi@DESKTOP-OGSHBH7>
2022-09-25 01:00:08 +0530

    Add project files.

---
+++ Admin_Dashboard/Controllers/ProductApnaController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin_Dashboard.Controllers
+{
+    public class ProductApnaController : Controller
+    {
+        // GET: ProductApnaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductApnaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductApnaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductApnaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductApnaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductApnaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductApnaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductApnaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Admin_Dashboard/Models/ProductApna.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Admin_Dashboard.Models
+{
+    [Table("Product_Apna")]
+    public partial class ProductApna
+    {
+        [Key]
+        [Column("Product_Id")]
+        public int ProductId { get; set; }
+        [Required]
+
+        [Column("Product_Name")]
+        [StringLength(50)]
+
+        public string ProductName { get; set; }
+        [Required]
+        [Column("Product_Categ")]
+        [StringLength(50)]
+
+        public string ProductCateg { get; set; }
+        [Required]
+        [Column("Product_ExpiryDate")]
+        [StringLength(50)]
+
+        public string ProductExpiryDate { get; set; }
+        [Required]
+        [Column("Product_Quility")]
+
+        [StringLength(50)]
+        public string ProductQuility { get; set; }
+        [Required]
+
+        [Column("Product_No_of_Stock")]
+        [StringLength(50)]
+        public string ProductNoOfStock { get; set; }
+    }
+}
